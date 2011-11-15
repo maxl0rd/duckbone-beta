285da730345d03a56113f168f64ec9730139f7e4
John Mileham <jmileham@gmail.com>
2011-11-15 11:31:12 -0500

    More liberal date validator

--- vendor/assets/javascripts/duckbone/form_validations.js
+++ vendor/assets/javascripts/duckbone/form_validations.js
@@ -1,29 +1,28 @@
 (function() {
-  
+
   // These validators can be used with any form field by including the fields options
   // fields: {
   //   amount: {
   //     modelAttribute: 'amount',
   //     validate: 'money'
-  //   } 
+  //   }
   // }
-  
+
   // Thx to andyet/happy.js for a few of these
-  
+
   Duckbone.forms = Duckbone.forms || {};
   Duckbone.forms.validators = Duckbone.forms.validators || {};
-  
+
   _.extend(Duckbone.forms.validators, {
-    
+
     required: function(val) {
       return !(val == undefined || val == null || val == "");
     },
     USPhone: function (val) {
       return /^\(?(\d{3})\)?[\- ]?\d{3}[\- ]?\d{4}$/.test(val)
     },
-    // matches mm/dd/yyyy (requires leading 0's (which may be a bit silly, what do you think?)
     date: function (val) {
-      return /^(?:0[1-9]|1[0-2])\/(?:0[1-9]|[12][0-9]|3[01])\/(?:\d{4})/.test(val);
+      return /^(?:0?[1-9]|1[0-2])\/(?:0?[1-9]|[12][0-9]|3[01])\/\d{2}?\d{2}$/.test(val);
     },
     email: function (val) {
       return /^(?:\w+\.?)*\w+@(?:\w+\.)+\w+$/.test(val);
@@ -31,7 +30,7 @@
     numeric: function (val) {
       return (parseFloat(val) != NaN);
     }
-    
+
   });
-  
+
 }).call(this);
